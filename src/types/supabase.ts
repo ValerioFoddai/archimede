818e468b596098fbb91aae9a36c1e7fc277f22df
Valerio Foddai <valerio@foddai.com>
2025-01-09 20:25:13 +0100

    fix

---
+++ src/types/supabase.ts
@@ -0,0 +1,48 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      user_tags: {
+        Row: {
+          id: string
+          user_id: string
+          name: string
+          created_at: string
+        }
+        Insert: {
+          id?: string
+          user_id: string
+          name: string
+          created_at?: string
+        }
+        Update: {
+          id?: string
+          user_id?: string
+          name?: string
+          created_at?: string
+        }
+      }
+      transaction_tags: {
+        Row: {
+          transaction_id: string
+          tag_id: string
+        }
+        Insert: {
+          transaction_id: string
+          tag_id: string
+        }
+        Update: {
+          transaction_id?: string
+          tag_id?: string
+        }
+      }
+    }
+  }
+}
