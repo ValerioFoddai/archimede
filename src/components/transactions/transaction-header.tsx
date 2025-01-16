46d0cf13f61aa3de735f56047c9cff4088657f3d
Valerio Foddai <valerio@foddai.com>
2025-01-16 22:07:47 +0100

    Swap Button Transactions

--- src/components/transactions/transaction-header.tsx
+++ src/components/transactions/transaction-header.tsx
@@ -36,14 +36,14 @@ export function TransactionHeader({
         <Button variant="outline" size="icon" onClick={onApplyRules}>
           <Bot className="h-4 w-4" />
         </Button>
-        <Button variant="outline" size="icon" asChild>
-          <Link to="/transactions/import">
-            <Upload className="h-4 w-4" />
-          </Link>
+        <Button variant="outline" size="icon" onClick={onAddTransaction}>
+          <Plus className="h-4 w-4" />
         </Button>
-        <Button onClick={onAddTransaction}>
-          <Plus className="mr-2 h-4 w-4" />
-          Add Transaction
+        <Button asChild>
+          <Link to="/transactions/import">
+            <Upload className="mr-2 h-4 w-4" />
+            Import Transactions
+          </Link>
         </Button>
       </div>
     </div>
