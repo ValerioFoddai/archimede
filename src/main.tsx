98eefc32b2b8ef9cd0b6fcd7708d2fd837ee5ce0
Valerio Foddai <valerio@foddai.com>
2025-01-24 21:01:23 +0100

    fix vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Analytics debug={false} mode={import.meta.env.DEV ? 'development' : 'production'} />
   </React.StrictMode>,
 )
